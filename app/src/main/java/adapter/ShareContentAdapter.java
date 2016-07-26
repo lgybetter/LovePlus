61156dad7e70a9d57a34f024190d5dcca6d76f6c
lgybetter <437675103@qq.com>
2016-07-26 18:37:52 +0800

    Finish the wrong Content

--- app/src/main/java/adapter/ShareContentAdapter.java
+++ app/src/main/java/adapter/ShareContentAdapter.java
@@ -6,12 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.example.administrator.loveplus.R;
-
 import java.util.List;
-
-import beanclass.MessageBeanClass;
 import beanclass.ShareContentBeanClass;
 
 /**
--- app/src/main/java/fragment/HomeFragment.java
+++ app/src/main/java/fragment/HomeFragment.java
@@ -7,12 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.administrator.loveplus.R;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import adapter.ShareContentAdapter;
 import beanclass.ShareContentBeanClass;
 import customview.DividerItemDecoration;
@@ -20,42 +17,47 @@ import customview.DividerItemDecoration;
 /**
  * Created by Administrator on 2016/7/21.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment{
     /**
      *测试数据
      */
     private String[] fromNameArr = {"TOM","BOB","KATE"};
     private String[] textArr = {"It is a nice man!","So funny it is","Come to here"};
-    private RecyclerView recyclerView;
-    private View homeView;
-    private ShareContentAdapter adapter;
-    private ShareContentBeanClass shareContentBeanClass;
-    private List<ShareContentBeanClass> shareContentList = new ArrayList<>();
+//    private int[] imageSource = {
+//            R.drawable.vp_1,
+//            R.drawable.vp_2,
+//            R.drawable.vp_3,
+//            R.drawable.vp_4,
+//            R.drawable.vp_5
+//    };
+//    private RecyclerView recyclerView;
+//    private ShareContentAdapter adapter;
+//    private ShareContentBeanClass shareContentBeanClass;
+//    private List<ShareContentBeanClass> shareContentList = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         super.onCreateView(inflater, container, savedInstanceState);
-        homeView = inflater.inflate(R.layout.activity_tab_home, container,false);
-        initView();
-        initData();
-        adapter = new ShareContentAdapter(getContext(),shareContentList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL_LIST));
-        recyclerView.setAdapter(adapter);
+        View homeView = inflater.inflate(R.layout.activity_tab_home, container,false);
+//        initView();
+//        initData();
+//        adapter = new ShareContentAdapter(getContext(),shareContentList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL_LIST));
+//        recyclerView.setAdapter(adapter);
         return homeView;
     }
 
-    private void initData() {
-        shareContentList.clear();
-        for(int i = 0; i < fromNameArr.length; i ++) {
-            shareContentBeanClass = new ShareContentBeanClass(fromNameArr[i],textArr[i]);
-            shareContentList.add(shareContentBeanClass);
-        }
-    }
-
-    private void initView() {
-        recyclerView = (RecyclerView) homeView.findViewById(R.id.rv_share_content);
-    }
-
+//    private void initData() {
+//        shareContentList.clear();
+//        for(int i = 0; i < fromNameArr.length; i ++) {
+//            shareContentBeanClass = new ShareContentBeanClass(fromNameArr[i],textArr[i]);
+//            shareContentList.add(shareContentBeanClass);
+//        }
+//    }
+//
+//    private void initView() {
+//        recyclerView = (RecyclerView) homeView.findViewById(R.id.rv_share_content);
+//    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
